d15f1a604d85be4b797396b939ee6ceca0b3528c
coolblast-coder <lyapunovantik@gmail.com>
2022-12-02 08:49:25 -0700

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool IsLoggedIn() {
+	string username, password, un, pw;
+	cout << "Enter username: ";
+	cin >> username;
+	cout << "Enter password: ";
+	cin >> password;
+
+	ifstream read("data//"+username+".txt");
+	getline(read, un);
+	getline(read, pw);
+
+	if (un == username && pw == password) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
